32f74cb2856ae100e2393624965cb0fc176f43d2
Andy <admazzola@gmail.com>
2016-04-22 02:21:02 -0400

    cleaned out old comments

--- src/com/starflask/networking/NetworkUtils.scala
+++ src/com/starflask/networking/NetworkUtils.scala
@@ -27,47 +27,20 @@ object NetworkUtils {  //like static
         
      }
   
-   
-     //make this a tuple of twos ...??
+    
      val allMessageTypes = List(classOf[NetworkMessage],classOf[ JoinServerAction ], classOf[ MoveAction ])
      
 
    def registerMessageTypes(networkInterface: Any, listener: MessageListener[Any]) = networkInterface match 
   {
-        //case s: Server => Serializer.registerClass(classOf[NetworkMessage]);   s.addMessageListener( listener , classOf[NetworkMessage]);
-        case s: Server => allMessageTypes.map{t => Serializer.registerClass( t ) } ; allMessageTypes.map{t => s.addMessageListener( listener , t) } ;  
+         case s: Server => allMessageTypes.map{t => Serializer.registerClass( t ) } ; allMessageTypes.map{t => s.addMessageListener( listener , t) } ;  
                   
         case c: Client => allMessageTypes.map{t => Serializer.registerClass( t   ) } ; allMessageTypes.map{t => c.addMessageListener( listener , t) } ;  
                     
         case _ => println("invalid type"); 
             
   }
-     
-     ///ARGGGGGG 
-      //make a diff serializer for each message type 
-     
-     
-     /*case class CustomSerializer[T <: Object :ClassTag](  factory: () => T) extends Serializer  {
-
-        
-     val x = classOf[JoinServerAction]
-     
-    override def   readObject( data:ByteBuffer,  c:Class[T] ):  T  =  c.getClass  match{
-        case x =>  factory() 
-    }
-
-     override def  writeObject(buffer: ByteBuffer , o: Object) = o.getClass match  {
-       
-       case j   =>  buffer.putLong(1)
-       
-       
-        /*UUID uuid = (UUID) object;
-        
-        buffer.putLong(uuid.getMostSignificantBits());
-        buffer.putLong(uuid.getLeastSignificantBits());*/
-    }
-
-}*/
+    
    
     
    
